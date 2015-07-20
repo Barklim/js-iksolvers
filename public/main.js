a16427214dd9d3f481ceb1680711b61465809a9e
Lucas Doyle <lucas.p.doyle@gmail.com>
2015-07-19 22:48:34 -0700

    can move dofvalues in collada!

--- public/main.js
+++ public/main.js
@@ -3,14 +3,13 @@ zip.workerScriptsPath = '/public/vendor/zip/WebContent/';
 var Module = {
     onRuntimeInitialized: function() {
 
+        var gui = new dat.GUI();
 
-        var ForwardKin = function() {
-            //$('#root').append('<pre>' + JSON.stringify(Module['solverInfo'], null, 2) + '</pre>');
-            //$('#root').append('<pre>' + 'getNumJoints = ' +  + '</pre>');
-            this.numJoints = Module.getNumJoints();
-            for (var i = 0; i < this.numJoints; i++) {
-                this['j' + i] = 0.0
-            }
+        var Solver = function() {
+
+            var self = this;
+
+            var numJoints = Module.getNumJoints();
 
             var container;
 
@@ -18,55 +17,65 @@ var Module = {
             var particleLight;
             var dae;
 
-            var kinematics;
+            this.kinematics = null;
 
             var _loadCollada = function(url, scale) {
                 scale = scale || 1.0;
 
                 var loader = new THREE.ColladaLoader();
                 loader.options.convertUpAxis = true;
-                loader.load( url, function ( collada ) {
+                loader.load(url, function(collada ) {
 
                     dae = collada.scene;
 
-                    dae.traverse( function ( child ) {
+                    dae.traverse(function(child ) {
 
-                        if ( child instanceof THREE.Mesh ) {
+                        if (child instanceof THREE.Mesh) {
 
                             child.geometry.computeFaceNormals();
                             child.material.shading = THREE.FlatShading;
 
                         }
 
-                    } );
+                    });
 
                     dae.scale.x = dae.scale.y = dae.scale.z = 5.0;
                     dae.updateMatrix();
 
                     kinematics = collada.kinematics;
 
-                    init();
-                    animate();
+                    for (var i = 0; i < kinematics.joints.length; i++) {
+                        (function(i) {
+                            var joint = kinematics.joints[i];
+                            self['j' + i] = joint.zeroPosition;
+                            var controller = gui.add(self, 'j' + i, joint.limits.min, joint.limits.max);
+                            controller.onChange(function(value) {
+                                kinematics.setJointValue(i, value);
+                            });
+                        })(i);
+                    }
 
-                } );
+                init();
+                animate();
+
+                });
             };
 
             var sceneURL = '/' + Module.solverInfo.scene;
             var colladaExtension = sceneURL.split('.').pop();
 
-            if (colladaExtension == 'dae' ) {
+            if (colladaExtension == 'dae') {
                 _loadCollada(sceneURL);
-            }
-            else if (colladaExtension == 'zae') {
-                zip.createReader(new zip.HttpReader(sceneURL), function (zipReader) {
-                    zipReader.getEntries(function (entries) {
+            } else if (colladaExtension == 'zae') {
+                zip.createReader(new zip.HttpReader(sceneURL), function(zipReader) {
+                    zipReader.getEntries(function(entries) {
 
                         var hasLoadedCollada = false;
 
                         if (entries.length) {
-                            entries.forEach(function (entry) {
+                            entries.forEach(function(entry) {
                                 if (!hasLoadedCollada && entry.filename.split('.').pop() == 'dae') {
-                                    entry.getData(new zip.BlobWriter('text/plain'), function (data) {
+                                    entry.getData(new zip.BlobWriter('text/plain'), function(data) {
                                         zipReader.close();
                                         _loadCollada(URL.createObjectURL(data));
                                         hasLoadedCollada = true;
@@ -75,18 +84,18 @@ var Module = {
                             });
                         }
                     });
-                }, function () {
+                }, function() {
                     console.warn('Drawable: problem loading ' + zaeUrl);
                 });
             }
 
             function init() {
 
-                container = document.createElement( 'div' );
-                document.body.appendChild( container );
+                container = document.createElement('div');
+                document.body.appendChild(container);
 
-                camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-                camera.position.set( 2, 2, 3 );
+                camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+                camera.position.set(2, 2, 3);
 
                 scene = new THREE.Scene();
 
@@ -95,49 +104,49 @@ var Module = {
                 var size = 14, step = 1;
 
                 var geometry = new THREE.Geometry();
-                var material = new THREE.LineBasicMaterial( { color: 0x303030 } );
+                var material = new THREE.LineBasicMaterial({ color: 0x303030 });
 
-                for ( var i = - size; i <= size; i += step ) {
+                for (var i = - size; i <= size; i += step) {
 
-                    geometry.vertices.push( new THREE.Vector3( - size, - 0.04, i ) );
-                    geometry.vertices.push( new THREE.Vector3(   size, - 0.04, i ) );
+                    geometry.vertices.push(new THREE.Vector3(- size, - 0.04, i));
+                    geometry.vertices.push(new THREE.Vector3(size, - 0.04, i));
 
-                    geometry.vertices.push( new THREE.Vector3( i, - 0.04, - size ) );
-                    geometry.vertices.push( new THREE.Vector3( i, - 0.04,   size ) );
+                    geometry.vertices.push(new THREE.Vector3(i, - 0.04, - size));
+                    geometry.vertices.push(new THREE.Vector3(i, - 0.04, size));
 
                 }
 
-                var line = new THREE.Line( geometry, material, THREE.LinePieces );
-                scene.add( line );
+                var line = new THREE.Line(geometry, material, THREE.LinePieces);
+                scene.add(line);
 
                 // Add the COLLADA
 
-                scene.add( dae );
+                scene.add(dae);
 
-                particleLight = new THREE.Mesh( new THREE.SphereGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
-                scene.add( particleLight );
+                particleLight = new THREE.Mesh(new THREE.SphereGeometry(4, 8, 8), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+                scene.add(particleLight);
 
                 // Lights
 
-                var directionalLight = new THREE.HemisphereLight( 0xffeeee, 0x111122 );
+                var directionalLight = new THREE.HemisphereLight(0xffeeee, 0x111122);
                 directionalLight.position.x = Math.random() - 0.5;
                 directionalLight.position.y = Math.random() - 0.5;
                 directionalLight.position.z = Math.random() - 0.5;
                 directionalLight.position.normalize();
-                scene.add( directionalLight );
+                scene.add(directionalLight);
 
-                var pointLight = new THREE.PointLight( 0xffffff, 0.5 );
-                particleLight.add( pointLight );
+                var pointLight = new THREE.PointLight(0xffffff, 0.5);
+                particleLight.add(pointLight);
 
                 renderer = new THREE.WebGLRenderer();
-                renderer.setPixelRatio( window.devicePixelRatio );
-                renderer.setSize( window.innerWidth, window.innerHeight );
-                container.appendChild( renderer.domElement );
+                renderer.setPixelRatio(window.devicePixelRatio);
+                renderer.setSize(window.innerWidth, window.innerHeight);
+                container.appendChild(renderer.domElement);
 
 
                 //
 
-                window.addEventListener( 'resize', onWindowResize, false );
+                window.addEventListener('resize', onWindowResize, false);
 
             }
 
@@ -146,7 +155,7 @@ var Module = {
                 camera.aspect = window.innerWidth / window.innerHeight;
                 camera.updateProjectionMatrix();
 
-                renderer.setSize( window.innerWidth, window.innerHeight );
+                renderer.setSize(window.innerWidth, window.innerHeight);
 
             }
 
@@ -154,7 +163,7 @@ var Module = {
 
             function animate() {
 
-                requestAnimationFrame( animate );
+                requestAnimationFrame(animate);
 
                 render();
 
@@ -164,29 +173,22 @@ var Module = {
 
                 var timer = Date.now() * 0.0001;
 
-                camera.position.x = Math.cos( timer ) * 17;
+                camera.position.x = Math.cos(timer) * 17;
                 camera.position.y = 10;
-                camera.position.z = Math.sin( timer ) * 17;
+                camera.position.z = Math.sin(timer) * 17;
 
-                camera.lookAt( scene.position );
+                camera.lookAt(scene.position);
 
-                particleLight.position.x = Math.sin( timer * 4 ) * 3009;
-                particleLight.position.y = Math.cos( timer * 5 ) * 4000;
-                particleLight.position.z = Math.cos( timer * 4 ) * 3009;
+                particleLight.position.x = Math.sin(timer * 4) * 3009;
+                particleLight.position.y = Math.cos(timer * 5) * 4000;
+                particleLight.position.z = Math.cos(timer * 4) * 3009;
 
-                renderer.render( scene, camera );
+                renderer.render(scene, camera);
 
             }
 
-        }
+        };
 
-        var fk = new ForwardKin();
-        var gui = new dat.GUI();
-        for (var i = 0; i < fk.numJoints; i++) {
-            gui.add(fk, 'j' + i)
-        }
-
-
-
+        var s = new Solver();
     }
 };
